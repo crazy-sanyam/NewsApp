8cff6ca287a5dbe506ca0e659ae4191ff1641143
Sanyam <sanyamjain9719@gmail.com>
2021-09-05 09:25:26 +0530

    NewsApp: learning REACT with code with harry

---
+++ src/components/NewsItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=2306920cd072400b848397c2bbdb38b1";
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
+}
+
+    render() {
+        let { title, description,imageUrl,newsUrl } = this.props;
+        return (
+            <div className="container my-3">
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title} . . .</h5>
+                        <p className="card-text">{description} . . .</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default NewsItem
