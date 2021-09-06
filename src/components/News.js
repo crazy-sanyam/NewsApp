7777826e7bd56ff139f647149f376b5717b724ea
Sanyam <sanyamjain9719@gmail.com>
2021-09-06 16:38:40 +0530

    added category of news feature

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+    static defaultProps={
+        country: 'in',
+        pageSize:8,
+        category:'general',
+    }
+    static propTypes={
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
 
 
     constructor() {
@@ -10,51 +23,62 @@ export class News extends Component {
             articles: [],
             loading: false,
             page: 1,
-           
+
         }
     }
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=2306920cd072400b848397c2bbdb38b1&page=1&pageSize=9";
+        this.setState({ loading: true })
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         // console.log(parsedData);
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false
+        })
     }
 
 
     handlePrevClick = async () => {
+        this.setState({ loading: true })
         console.log("Previous page");
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page - 1}&pageSize=9`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({
-            page:  this.state.page + 1,
-            articles: parsedData.articles 
+            page: this.state.page - 1,
+            articles: parsedData.articles,
+            loading: false
         })
     }
     handleNextClick = async () => {
-
+        this.setState({ loading: true })
         console.log("Next Page");
-        if(this.state.page + 1>Math.ceil(this.state.totalResults/9)){
-
-        }else{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page + 1}&pageSize=9`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles
-        })}
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
+        } else {
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles,
+                loading: false
+            })
+        }
     }
     render() {
 
         return (
+
             <div div >
-                <h1>NewsMonkey - Top Headlines</h1>
+                <h1 className="text-center" style={{margin:'30px,0px'}}>NewsMonkey - Top Headlines</h1>
+                {this.state.loading && <Spinner />}
                 <div className="row">
-                    {this.state.articles.map((element) => {
+                    {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
                             <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
@@ -65,7 +89,7 @@ export class News extends Component {
 
                         <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
                     </svg> Previous</button>
-                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
                     </svg></button>
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -14,7 +14,7 @@ export class NewsItem extends Component {
         let { title, description,imageUrl,newsUrl } = this.props;
         return (
             <div className="container my-3">
-                <div className="card" style={{ width: "18rem" }}>
+                <div className="card">
                     <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
