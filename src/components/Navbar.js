ee13708d5521586ba1bd9fe63197bd896e4c2d08
Sanyam <sanyamjain9719@gmail.com>
2021-09-08 09:40:36 +0530

    added author date and source to newsapp learnt proptypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
     render() {
@@ -13,34 +13,22 @@ export default class Navbar extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Business">Business</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Entertainment">Entertainment</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/General">General</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Health">Health</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Science">Science</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Sports">Sports</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Technology">Technology</Link>
-                                </li>
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Home</Link></li>
 
-
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/Business">Business</Link></li>
+                                
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/Entertainment">Entertainment</Link></li>
+                                
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/General">General</Link></li>
+                                
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/Health">Health</Link></li>
+                                
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/Science">Science</Link></li>
+                                
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/Sports">Sports</Link></li>
+                                
+                                <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/Technology">Technology</Link></li>
                             </ul>
-
                         </div>
                     </div>
                 </nav>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,12 +11,12 @@ export class NewsItem extends Component {
 
     render() {
 
-        let { title, description, imageUrl, newsUrl, author, date } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="container my-3">
                 <div className="card">
-                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    hello my name is sam
+                <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+                    {source}
                     <span className="visually-hidden">unread messages</span>
                 </span>
                     <img src={imageUrl} className="card-img-top" alt="..." />
