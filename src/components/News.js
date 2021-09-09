861ac5f82fcbba0f6af101cba54bc0cd8ccf1ad0
Sanyam <sanyamjain9719@gmail.com>
2021-09-09 09:50:46 +0530

    Adding Infinite ScrollBar in place of the next-Previous button

--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
-
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
     static defaultProps = {
@@ -22,13 +22,14 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
     }
     async updateNews() {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
@@ -55,28 +56,43 @@ export class News extends Component {
         });
         this.updateNews();
     }
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 });
+        // this.setState({ loading: true })
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+
+        })
+    }
     render() {
         return (
-            <div div >
+            <>
                 <h1 className="text-center" style={{ margin: '30px,30px' }}>NewsMonkey - Top Headlines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                        </div>
-                    })}
-                </div>
-                <div className=" d-flex justify-content-between container">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                <InfiniteScroll
+                    dataLength={this.state.articles.length} //This is important field to render the next data
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
 
-                        <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
-                    </svg> Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-                    </svg></button>
-                </div>
-            </div>
+                >
+
+                    <div className="container">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
+            </>
         )
 
 
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Spinner extends Component {
     render() {
         return (
-            <div>
+            <div className="mx-3 my-3">
                 <div className="d-flex justify-content-center">
                     <div className="spinner-border" role="status">
                         <span className="sr-only"> </span>
