33c4179120a0166f3dddf3cdd9b4f209b47c6b34
Sanyam <sanyamjain9719@gmail.com>
2021-09-09 08:50:17 +0530

    made my some code acc to DRY principle

--- src/components/News.js
+++ src/components/News.js
@@ -26,61 +26,44 @@ export class News extends Component {
             page: 1
         }
     }
-    async componentDidMount() {
+    async updateNews() {
         this.setState({ loading: true })
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-        // console.log(parsedData);
-
+        console.log(parsedData);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         })
     }
+    async componentDidMount() {
+        this.updateNews();
+    }
 
 
     handlePrevClick = async () => {
-        this.setState({ loading: true })
-        console.log("Previous page");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
         this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+            page: this.state.page - 1
+        });
+        this.updateNews();
     }
     handleNextClick = async () => {
-        this.setState({ loading: true })
-        console.log("Next Page");
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
-        } else {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2306920cd072400b848397c2bbdb38b1&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            let data = await fetch(url);
-            let parsedData = await data.json();
-            console.log(parsedData);
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            })
-        }
+        this.setState({
+            page: this.state.page + 1
+        });
+        this.updateNews();
     }
     render() {
-
         return (
-
             <div div >
                 <h1 className="text-center" style={{ margin: '30px,30px' }}>NewsMonkey - Top Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })}
                 </div>
@@ -99,5 +82,6 @@ export class News extends Component {
 
     }
 }
+
 
 export default News
